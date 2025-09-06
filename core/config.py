16e70fc14035b00927ff4b69ded2ce563e6a20c4
single_ptilopsis <ptilopsis_program@foxmail.com>
2025-09-07 01:44:52 +0800

    fix: 修复配置文件后缀名判断中的错误

--- core/config.py
+++ core/config.py
@@ -18,7 +18,7 @@ class SystemConfig(BaseModel, extra="ignore"):
 def read_config[T](path: Path, obj: type[T]) -> T:
     if path.exists():
         with path.open(encoding="utf8") as f:
-            return obj.model_validate((tomlkit.load(f) if path.suffix == "toml" else yaml.safe_load(f)) or {})  # type: ignore
+            return obj.model_validate((tomlkit.load(f) if path.suffix == ".toml" else yaml.safe_load(f)) or {})  # type: ignore
     else:
         return obj.model_validate({})  # type: ignore
 
@@ -30,7 +30,7 @@ system_config = read_config(CONFIG_PATH, SystemConfig)
 
 def write_config(config, path: Path = CONFIG_PATH):
     with Path(path).open(mode="w", encoding="utf8") as f:
-        if path.suffix == "toml":
+        if path.suffix == ".toml":
             tomlkit.dump(config.model_dump(), f)
         else:
             yaml.dump(config.model_dump(), f, allow_unicode=True, indent=2)
