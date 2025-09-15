17e82f158d880b613df5339e3e7d1eed96612dc6
single_ptilopsis <ptilopsis_program@foxmail.com>
2025-09-15 11:07:03 +0800

    feat: 添加系统日志

--- src/http/user.py
+++ src/http/user.py
@@ -1,31 +1,22 @@
-from __future__ import annotations
+from __future__ import annotations  # noqa: I001
 
 import asyncio
-import json
-from typing import TYPE_CHECKING, Literal
+from typing import Literal
 
-from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
 from src.constance import BDUSS_MOSAIC, STOKEN_MOSAIC
 from src.rule.rule import RuleInfo, Rules
 from src.user.manager import User, UserManager
-from src.util.logging import JSON_LOG_DIR, LogEvent, LogEventData, LogRecorder, system_logger
 
 from ..server import (
     BaseResponse,
     app,
     current_user_depends,
-    parse_token,
     system_access_depends,
 )
 
-if TYPE_CHECKING:
-    from loguru import Message
-
 # 不要将下方的导入移动到TYPE_CHECKING中，否则会导致fastapi无法正确生处理请求
-
-from fastapi import Request  # noqa: TC002
 
 from src.rule.rule_set import RuleSetConfig  # noqa: TC001
 from src.user.config import ForumConfig, ProcessConfig, UserPermission  # noqa: TC001
@@ -160,99 +151,3 @@ async def confirm_operation(user: current_user_depends, request: ConfirmRequest)
     else:
         asyncio.create_task(confirm_many(user, request.pid, request.action))
         return BaseResponse(data=True, message="正在批量执行操作，请稍后查看结果")
-
-
-class LogData(BaseModel):
-    message: str
-    name: str
-    level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-    extra: dict
-
-    @staticmethod
-    def from_message(message: Message) -> LogData:
-        return LogData(
-            message=message.rstrip("\n"),
-            name=message.record["extra"].get("name", "unknown"),
-            level=message.record["level"].name.upper(),  # type: ignore
-            extra={k: v for k, v in message.record["extra"].items() if k != "name"},
-        )
-
-
-@app.get("/api/log/get_list", tags=["log"])
-async def get_log_list(user: current_user_depends) -> BaseResponse[list[str]]:
-    files = sorted(JSON_LOG_DIR.glob("webtm_*.json"), key=lambda x: x.stat().st_mtime, reverse=True)
-    return BaseResponse(data=[i.stem for i in files])
-
-
-@app.get("/api/log/user_realtime", tags=["log"])
-async def user_realtime_log(request: Request, token: str) -> StreamingResponse:
-    user, system_access = await parse_token(token)
-    name = f"user.{user.username}"
-
-    records = [LogData.from_message(i) for i in LogRecorder.get_records(name)]
-    queue = asyncio.Queue()
-
-    for record in records:
-        await queue.put(record)
-
-    async def log_listener(data: LogEventData):
-        try:
-            if data.name != name:
-                return
-
-            log_data = LogData.from_message(data.message)
-            await queue.put(log_data)
-        except Exception:
-            system_logger.exception("推送日志时发生错误")
-
-    listener = LogEvent.on(log_listener)
-
-    async def event_generator():
-        try:
-            while True:
-                log = await queue.get()
-                yield f"data: {json.dumps(log.model_dump(), ensure_ascii=False)}\n\n"
-                if await request.is_disconnected():
-                    break
-        except Exception:
-            system_logger.exception("推送日志时发生错误")
-        finally:
-            listener.un_register()
-
-    return StreamingResponse(event_generator(), media_type="text/event-stream")
-
-
-@app.get("/api/log/get_user", tags=["log"])
-async def get_user_log(user: current_user_depends, file: str) -> BaseResponse[list[LogData]]:
-    path = JSON_LOG_DIR / f"{file}.json"
-    if not path.exists() or not path.is_file():
-        return BaseResponse(data=[], message="日志文件不存在", code=400)
-
-    logs = []
-    user_name = f"user.{user.username}"
-    with path.open("r", encoding="utf-8") as f:
-        for line in f:
-            try:
-                log = json.loads(line)
-                extra = log["record"]["extra"]
-                if "name" in extra:
-                    name = extra["name"]
-                    del extra["name"]
-                else:
-                    name = "unknown"
-
-                if name != user_name:
-                    continue
-
-                logs.append(
-                    LogData(
-                        message=log["text"].rstrip("\n"),
-                        name=name,
-                        level=log["record"]["level"]["name"].upper(),
-                        extra=extra,
-                    )
-                )
-            except Exception:
-                continue
-
-    return BaseResponse(data=logs)
