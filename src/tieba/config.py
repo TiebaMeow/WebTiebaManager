636de1d5d33bf4c7f63a32b7cd9bf90ffd583da3
single_ptilopsis <ptilopsis_program@foxmail.com>
2025-09-17 09:47:49 +0800

    refactor: 移除 ScanConfig 中未使用的配置项

--- src/tieba/config.py
+++ src/tieba/config.py
@@ -4,8 +4,6 @@ from pydantic import BaseModel
 class ScanConfig(BaseModel, extra="ignore"):
     loop_cd: int = 10
     query_cd: float = 0.05
-    pc_query_cd: float = 2
-    operate_cd: float = 0.5
     thread_page_forward: int = 1
     post_page_forward: int = 1
     post_page_backward: int = 1
